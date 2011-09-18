c8afd9347214259e53a77375b81c5dc2a2fe882f
Daniel Lowe <dl387@cam.ac.uk>
2011-09-18 22:06:42 +0100

    SpaceRemovalResult now returns the final input string (the space removed string) Allowed the special cases such as benzene sulfonamide to be used in combination with standard space removal e.g. benz ene sulfonamide is interpretable

--- src/main/java/org/bitbucket/dan2097/structureExtractor/SpaceRemovalResult.java
+++ src/main/java/org/bitbucket/dan2097/structureExtractor/SpaceRemovalResult.java
@@ -6,21 +6,47 @@ package org.bitbucket.dan2097.structureExtractor;
 import uk.ac.cam.ch.wwmm.opsin.ParseRulesResults;
 
 class SpaceRemovalResult{
+	final boolean success;
+	final ParseRulesResults parseRulesResults;
+	final String inputString;
+	final Integer spacesRemoved;
+
+	public SpaceRemovalResult(boolean success, ParseRulesResults prr, String inputString, Integer spacesRemoved) {
+		this.success = success;
+		this.spacesRemoved = spacesRemoved;
+		this.inputString = inputString;
+		this.parseRulesResults = prr;
+	}
+	
+	/**
+	 * Was space removal successful
+	 * @return
+	 */
 	boolean isSuccess() {
 		return success;
 	}
-	int getSpacesRemoved() {
-		return spacesRemoved;
-	}
+
+	/**
+	 * Null if success=false
+	 * @return
+	 */
 	ParseRulesResults getParseRulesResults() {
 		return parseRulesResults;
 	}
-	final boolean success;
-	final int spacesRemoved;
-	final ParseRulesResults parseRulesResults;
-	public SpaceRemovalResult(boolean success, int spacesRemoved, ParseRulesResults prr) {
-		this.success = success;
-		this.spacesRemoved = spacesRemoved;
-		this.parseRulesResults = prr;
+	
+	/**
+	 * Null if success=false
+	 * @return
+	 */
+	String getInputString() {
+		return inputString;
+	}
+
+	/**
+	 * Null if success=false
+	 * @return
+	 */
+	Integer getSpacesRemoved() {
+		return spacesRemoved;
 	}
 }
